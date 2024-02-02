545a08b82c3967c81127482cda1ba3e5e8c4a9c4
kewen <chenkewen2007@foxmail.com>
2024-02-02 17:30:44 +0800

    change

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
    > 代替rm执行删除命令，防止删除错误导致系统无法使用
 2. DeleteFile.sh
    > 删除指定文件夹的文件，并生成日志进行记录
+3. docker_install.sh
+   > docker一键安装脚本
+
+
+# Windows_script
+ > windows 相关脚本
--- docker_install.sh
+++ docker_install.sh
@@ -1,2 +1,34 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+# 手动安装Docker步骤可以参考Docker官方教程：
+
+#CentOS：https://docs.docker.com/engine/install/centos/
+#Debian：https://docs.docker.com/engine/install/debian/
+#Fedora：https://docs.docker.com/engine/install/fedora/
+#Ubuntu：https://docs.docker.com/engine/install/ubuntu/
+
+# 更换为阿里云源进行加速
+cat > /etc/docker/daemon.json << EOF
+{
+ "registry-mirrors": ["https://b9pmyelo.mirror.aliyuncs.com"]
+}
+EOF
+
+systemctl daemon-reload
+
+systemctl restart docker
+
+#需要更换其他源请参考以下链接
+
+#网易镜像加速：
+
+#http://hub-mirror.c.163.com
+
+#中国科技大学镜像加速：
+
+#https://docker.mirrors.ustc.edu.cn
+
+#腾讯云镜像加速：
+
+#https://mirror.ccs.tencentyun.com
